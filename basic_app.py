d94704620537a2099fb568b48f00fd282ea10701
Roelan <100556622+dakkkkkk@users.noreply.github.com>
2024-08-30 17:34:26 +0800

    Update basic_app.py

--- basic_app.py
+++ basic_app.py
@@ -15,21 +15,17 @@ elif selected_page == "About Me":
     st.title("About Me")
 
     htp = "https://github.com/dakkkkkk/CSIT342-Portfolio/blob/main/Untitled.png?raw=true"
-    # Centered image with name and detailed description
+    # Centered image with name and justified paragraphs
     st.markdown(f"""
         <div style="text-align: center;">
             <img src="{htp}" alt="logo" width="500">
-            <p style="font-size: 24px; color: black; font-weight: bold;">Roelan Amerila</p>
-            <p style="font-size: 16px; color: grey;">
+            <p style="font-size: 24px; color: black; font-weight: bold; text-align: center;">Roelan Amerila</p>
+            <p style="font-size: 16px; color: grey; text-align: justify;">
                 I'm a 4th-year college student studying at Cebu Institute of Technology - University. My course is Information Technology.
                 <br><br>
-                I have a strong passion for technology and programming, and I enjoy working on projects that involve software development and data analysis. 
+                I have a passion for technology and programming, and I enjoy working on projects that involve software development and data analysis. 
                 Throughout my studies, I have developed skills in various programming languages such as Python, Java, and JavaScript. I am particularly interested in 
                 web development and machine learning. 
-                <br><br>
-                In addition to my academic pursuits, I am actively involved in extracurricular activities and have participated in several hackathons and coding competitions. 
-                These experiences have helped me hone my problem-solving skills and work effectively in a team. I am always eager to learn and take on new challenges, 
-                and I look forward to contributing to the tech industry in the future.
             </p>
         </div>
     """, unsafe_allow_html=True)
