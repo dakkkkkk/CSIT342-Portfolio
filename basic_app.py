6adc16820dcf95ef23cad7ebcb8c47413257f72a
Roelan <100556622+dakkkkkk@users.noreply.github.com>
2024-08-30 17:21:27 +0800

    Update basic_app.py

--- basic_app.py
+++ basic_app.py
@@ -1,38 +1,25 @@
-
 import streamlit as st
 
-
+# Main title
 st.title("My Autobiography and Portfolio")
 
+# Navigation
 pages = ["Home", "About Me", "Portfolio", "Autobiography"]
-
 selected_page = st.selectbox("Navigate a page", pages)
 
+# Page content
 if selected_page == "Home":
     st.title("Home")
  
 elif selected_page == "About Me":
     st.title("About Me")
 
-htp = "https://github.com/dakkkkkk/CSIT342-Portfolio/blob/main/Untitled.png"
-st.image(htp, caption= 'logo', width=350)
+    # Add an image under About Me
+    htp = "https://github.com/dakkkkkk/CSIT342-Portfolio/blob/main/Untitled.png?raw=true"
+    st.image(htp, caption='logo', width=350)
   
 elif selected_page == "Portfolio":
     st.title("Portfolio")
    
 elif selected_page == "Autobiography":
     st.title("Autobiography")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
