cbb3b929f324d044dcb0fbd88046552a27040de8
Roelan <100556622+dakkkkkk@users.noreply.github.com>
2024-08-30 16:13:50 +0800

    Update basic_app.py

--- basic_app.py
+++ basic_app.py
@@ -7,3 +7,28 @@ pages = ["Home", "About Me", "Portfolio", "Autobiography"]
 
 selected_page = st.selectbox("Navigate a page", pages)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+st.markdown("""
+ <style>
+body{ 
+background-color: #f0f2f6;
+
+}
+ </style>
+    """, unsafe_allow_html=True)
+
