1880af91386eb0609110fa170f7be795034ccb15
Roelan <100556622+dakkkkkk@users.noreply.github.com>
2024-08-30 16:16:08 +0800

    Update basic_app.py

--- basic_app.py
+++ basic_app.py
@@ -1,6 +1,22 @@
 
 import streamlit as st
 
+st.markdown("""
+ <style>
+ 
+body{ 
+background-color: #f0f2f6;
+
+}
+
+.stTitle{
+
+font-size: 18px;
+
+}
+ </style>
+    """, unsafe_allow_html=True)
+
 st.title("My Autobiography and Portfolio")
 
 pages = ["Home", "About Me", "Portfolio", "Autobiography"]
@@ -22,13 +38,4 @@ selected_page = st.selectbox("Navigate a page", pages)
 
 
 
-
-st.markdown("""
- <style>
-body{ 
-background-color: #f0f2f6;
-
-}
- </style>
-    """, unsafe_allow_html=True)
 
