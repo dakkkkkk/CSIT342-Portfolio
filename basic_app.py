a3c095ea0d9d8ccb3a62e2e5e4252fed4f2d40c8
Roelan <100556622+dakkkkkk@users.noreply.github.com>
2024-08-30 16:06:52 +0800

    Update basic_app.py

--- basic_app.py
+++ basic_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 st.title("My Autobiography and Portfolio")
 
-pages = ["Home", "About Me", "Portfolio, "Autobiography"]
+pages = ["Home", "About Me", "Portfolio", "Autobiography"]
 
-         
+selected_page = st.selectbox("Navigate a page", pages)
+
