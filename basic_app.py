4009029f4af7a368eb28722e827dcc47b4de1777
Roelan <100556622+dakkkkkk@users.noreply.github.com>
2024-08-30 16:32:02 +0800

    Update basic_app.py

--- basic_app.py
+++ basic_app.py
@@ -11,7 +11,7 @@ selected_page = st.selectbox("Navigate a page", pages)
 if selected_page == "Home":
     st.title("Home")
  
-elif selected_page == "About Me ":
+elif selected_page == "About Me":
     st.title("About Me")
   
 elif selected_page == "Portfolio":
@@ -19,7 +19,7 @@ elif selected_page == "Portfolio":
    
 elif selected_page == "Autobiography":
     st.title("Autobiography")
-    st.write("Learn more about this app.")
+
 
 
 
